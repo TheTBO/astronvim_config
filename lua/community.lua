47c65494b8753f28ac0532c3d1f719c382f1a70b
TheTBO <almujtaba.mhmd@gmail.com>
2025-03-31 19:41:25 +0800

    Add community packs

--- lua/community.lua
+++ lua/community.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
@@ -7,6 +7,21 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
+  -- { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
+  -- { import = "astrocommunity.completion.codeium-nvim" },
+  -- { import = "astrocommunity.completion.codeium-vim" },
+  { import = "astrocommunity.pack.php" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.prisma" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.laravel" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.cs" },
+  { import = "astrocommunity.remote-development.netman-nvim" },
 }
