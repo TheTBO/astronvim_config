aee78f0d87b2059ac78962cd089ee66f7b5e97c2
TheTBO <almujtaba.mhmd@gmail.com>
2025-03-31 18:54:17 +0800

    Update to V5

--- lua/community.lua
+++ lua/community.lua
@@ -1,3 +1,5 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -7,18 +9,4 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
-  { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
-  { import = "astrocommunity.completion.codeium-vim" },
-  { import = "astrocommunity.pack.php" },
-  { import = "astrocommunity.pack.typescript" },
-  { import = "astrocommunity.pack.html-css" },
-  { import = "astrocommunity.pack.docker" },
-  { import = "astrocommunity.pack.java" },
-  { import = "astrocommunity.pack.json" },
-  { import = "astrocommunity.pack.markdown" },
-  { import = "astrocommunity.pack.bash" },
-  { import = "astrocommunity.pack.prisma" },
-  { import = "astrocommunity.pack.tailwindcss" },
-  { import = "astrocommunity.pack.yaml" },
-  { import = "astrocommunity.remote-development.netman-nvim" },
 }
